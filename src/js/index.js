e93c551b98947693f7bb5788e8fafbe25422cc7b
alesha229 <dfyz123123123123@mail.ru>
2024-12-25 19:21:07 +0300

    Коментарии

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
 // radioQuestion - класс потомок question для вопросов с одним привильным вариантом ответа.
 // checkboxQuestion - класс потомок question для вопросов с несколькими правильными вариантами ответов.
 
+//Не используется т.к неясно для каких целей нужен
 function User() {
   this.getUserName = function () {
     //показываем поле
@@ -12,6 +13,7 @@ function User() {
 }
 
 function TestController() {
+  //конструктор контроллера
   this.currentQuestion = 0;
   this.sumResult = 0;
   this.service = new TestService();
@@ -20,41 +22,59 @@ function TestController() {
   this.isInit = null;
   this.isLast = false;
 }
+
 Object.assign(TestController.prototype, {
+  //Записываем методы в прототип функции конструктора, аналогично работе классов при создании экземпляра поля будут инициализированы в конструкторе
+  //при вызове методов они беруться из прототипа
+
+  //Инициализация контроллера
   init() {
     this.service.testInit().then((result) => (this.maxQuestion = result));
     this.questionFactory();
   },
 
+  //Очистка контейнеров
   clearQuestion() {
     document.getElementById("questionContainer").innerHTML = null;
     document.getElementById("answerContainer").innerHTML = null;
   },
 
   questionFactory() {
-    //метод для создания новых объектов вопросов в зависимости от типа вопроса,(checkBox,radio). Так же создает родителя question и настраивает наследование
+    //Метод для создания новых объектов вопроса используя сервис передаём туда текущий порядковый номер вопроса .then ждём разрешения промиса
     this.service.getNext(this.currentQuestion).then((data) => {
+      //Если объёкт вопроса не пустой удаляем содержимое по ссылке, очищаем DOM
       if (this.question != undefined) {
         this.clearQuestion();
         this.question = null;
       }
+      //При удачной загрузке текущий вопрос +1, сервис сам по себе обрабатывает ошибки.
       this.currentQuestion += 1;
+      //Создаем объект Question и записываем в контекст контроллера, передаем объект вопроса который нам вернул сервер, и ссылку на себя.
       this.question = new Question(data, this);
+      //Инициализируем объект вопроса.
       this.question.questionInit();
-      // this.question.deleteTimer();
     });
   },
 
   createNextQuestionObject(storyChunk) {
+    //Метод который записывает результат ответа на вопрос и показывает результаты или же запсукает фабрику в зависимости от окончания тестов
+
+    //Используется в другом месте для рендера кнопки(показать результаты)
     this.isLast = this.currentQuestion >= this.maxQuestion - 1;
+
+    //Пушим текущий чанк истории в общий массив с ответами пользователя и всей остальной инфой.
     if (storyChunk) this.story.push(storyChunk);
+
+    //В случае если у нас последний вопрос то в зависимости от этого показываем результаты или же запускаем фабрику
     if (this.currentQuestion >= this.maxQuestion) {
       this.showResult();
     } else {
       this.questionFactory();
     }
   },
+
   showResult() {
+    //Метод который рендерит результаты из истории ответов ничего интересного просто работа с DOM деревом.
     const result = document.getElementById("answerContainer");
     result.innerHTML = null;
     const questionContainer = document.getElementById("questionContainer");
@@ -67,7 +87,6 @@ Object.assign(TestController.prototype, {
     result.appendChild(rightAnswersContainer);
 
     this.story.forEach((element) => {
-      console.log(element);
       if (element.right) {
         this.sumResult += 1;
         rightSmile = '<span style="color: green;">✓</span>';
@@ -92,10 +111,11 @@ Object.assign(TestController.prototype, {
 });
 
 function Question(question, parent) {
-  this.questionData = question;
-  this.controller = parent;
-  console.log(question);
+  //Конструктор Question
+  this.questionData = question; //Записываем в контекст инфо полученное от сервера.
+  this.controller = parent; //Ссылка на контроллер
   this.storyAnswer = {
+    //Инициализация чанка истории, т.к получили от сервера поля question, answers, rightAnswers то можем удобно записать эти поля в конструкторе и позже дополнить.
     question: this.questionData.text,
     answers: this.questionData.options,
     rightAnswers: this.questionData.answers,
@@ -105,6 +125,7 @@ function Question(question, parent) {
 }
 Object.assign(Question.prototype, {
   questionInit() {
+    //Метод инициализации
     this.mutateOptions();
     this.mutateAnswers();
     this.createAnswers(this, this.questionData);
@@ -114,23 +135,30 @@ Object.assign(Question.prototype, {
     this.createTimer();
   },
   mutateOptions() {
+    //Мутируем строку с вариантами ответов в массив
     this.questionData.options = this.questionData.options.split("#;");
   },
 
   mutateAnswers() {
+    //Мутируем строку с ответами в массив
     this.questionData.answers = this.questionData.answers.split("#;");
   },
 
   isRadioQuestion() {
+    //В зависимости от того больше 1 элемента в массиве, возвращаем true/false
     return this.questionData.answers.length > 1 ? true : false;
   },
+
   createTimer() {
+    //Метод создания таймера
     if (this.timeOut != 0) {
+      //Если время таймера для вопроса !=0 то создаем таймер. Он там живет своей жизнью в замыкании
       this.timerInterval = setInterval(() => {
         const timer = document.getElementById("timer");
         timer.innerHTML = `Осталось времени: ${this.timeOut} секунд`;
         this.timeOut--;
         if (this.timeOut < 0) {
+          //Если время вышло то переходим к следующему вопросу
           this.handleNext();
         }
       }, 1000);
@@ -138,6 +166,7 @@ Object.assign(Question.prototype, {
   },
 
   deleteTimer(timerInterval) {
+    //Удаление таймера
     clearInterval(timerInterval);
     this.timerInterval = null;
     const timer = document.getElementById("timer");
@@ -145,13 +174,15 @@ Object.assign(Question.prototype, {
   },
 
   createTextAnswer() {
+    //Ничего интересного просто рендер текста вопроса в DOM
     this.options = this.questionData.options;
     const questionContainer = document.getElementById("questionContainer");
     questionContainer.innerHTML = `<div class="questionText mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 text-center">${this.questionData.text}</div>`;
   },
 
   createAnswers() {
-    console.log(this.controller);
+    //Метод isRadioQuestion возвращает true/false в зависимости от того несколько вариантов ответа или же один.
+    //Далее при помощи тернарного оператора выбираем какой тип объекта создать CheckBox или же Radio
     this.isRadioQuestion(this.questionData)
       ? (this.answer = new CheckBoxQuestion(
           this.questionData.options,
@@ -167,26 +198,36 @@ Object.assign(Question.prototype, {
           this.handleNext,
           this.controller
         ));
-    // Object.setPrototypeOf(this.answer, this);
+
+    //Инициализируем варианты ответов(рендер)
     this.answer.init();
   },
 
   handleNext() {
-    console.log(this.timerInterval);
+    //Оригинальный handleNext Question который мы позже переопределим в ребенке
+
+    //Если переходим дальше очищаем таймер
     this.deleteTimer(this.controller.question.timerInterval);
+
+    //Все ли вопросы правильные
     const allCorrectSelected = this.storyAnswer.rightAnswers.every((answer) =>
       this.storyAnswer.userAnswers.includes(answer)
     );
 
+    //Если есть 1 неправильный
     const hasIncorrectSelected = this.storyAnswer.userAnswers.some(
       (answer) => !this.storyAnswer.rightAnswers.includes(answer)
     );
 
+    //Оба должны быть true
     this.storyAnswer.right = allCorrectSelected && !hasIncorrectSelected;
+
+    //При помощи ссылки вызываем уже описаный метод
     this.controller.createNextQuestionObject(this.storyAnswer);
   },
+
   createNextbutton() {
-    console.log(this);
+    //Отрисовываем кнопку. Интересно что вызываем позже и дочернего элемента с указанием контекста.
     const answerContainer = document.getElementById("answerContainer");
     const nextButton = document.createElement("button");
     nextButton.classList =
@@ -204,32 +245,43 @@ Object.assign(Question.prototype, {
 });
 
 function RadioQuestion(
+  //Входные параметры конструктора
   options,
   isLast,
   question,
   handleNextCallback,
   controller
 ) {
+  //Инициализация конструтктора, конструктор делает нам контекст из входных параметров
   this.question = question;
-  Question.call(this, question);
-  this.controller = controller;
+  //super() //Инициализируем конструктор Question
+  Question.call(this, question, controller);
+  //Вызов функции конструктора не означает вызов методов или создания нового объекта, он просто "Добавляет" контекст к нашему
+  //Можем просто посмотреть что делает функция конструктор Question просто в .this записывает значения которые мы ему передали и у .this(контекста) RadioQuestion контекст расширен конструктором Question
   this.options = options;
   this.isLast = isLast;
-  this.handleNextCallback = handleNextCallback;
+  this.handleNextCallback = handleNextCallback; //при создании вопроса передали ссылку на метод handleNext
 }
-Object.assign(RadioQuestion.prototype, Question.prototype);
+Object.assign(RadioQuestion.prototype, Question.prototype); //берем два прототипа и мержим в один итог в первый аргумент записывается
 Object.assign(RadioQuestion.prototype, {
+  //аналогично только методы записываем как есть
   init() {
+    //инициализация типизированого вопроса
     this.renderQuestion();
     this.createNextbutton.call(this, this.question, this.questionData);
   },
+
   handleNext() {
+    //переопределенный метод handleNext
     document.querySelectorAll("input:checked").forEach((input, index) => {
       this.storyAnswer.userAnswers.push(input.parentNode.textContent);
     });
+    //Вызываем через колбек родительский одноименный метод handleNext
     this.handleNextCallback.call(this, this.storyAnswer);
   },
+
   renderQuestion() {
+    //Реднер вариантов ответа
     const answerContainer = document.getElementById("answerContainer");
     answerContainer.innerHTML = null;
     answerContainer.classList = "w-1/4 text-left mx-auto flex flex-col";
@@ -253,14 +305,14 @@ function CheckBoxQuestion(
   controller
 ) {
   this.question = question;
-  Question.call(this, question);
-  this.controller = controller;
+  Question.call(this, question, controller);
   this.options = options;
   this.isLast = isLast;
   this.handleNextCallback = handleNextCallback;
 }
-Object.assign(CheckBoxQuestion.prototype, Question.prototype);
+Object.assign(CheckBoxQuestion.prototype, Question.prototype); //берем два прототипа и мержим в один итог в первый аргумент записывается
 Object.assign(CheckBoxQuestion.prototype, {
+  //аналогично только методы записываем как есть
   init() {
     this.renderQuestion();
     this.createNextbutton.call(this, this.question, this.questionData);
@@ -288,16 +340,15 @@ Object.assign(CheckBoxQuestion.prototype, {
   },
 });
 
-function TestService() {}
+function TestService() {} //idk плохо ли что конструктор пустой но мы делаем по аналогии с классами и у класса конструктор может быть пустой
+
 Object.assign(TestService.prototype, {
+  //Методы сервиса отвечают только за работу с бэком, возвращают значение и не делают никаких побочных эффектов.
   async testInit() {
     try {
       const response = await fetch("http://localhost:8089/api/Test/TestInit");
       if (response.ok) {
         const data = await response.json();
-        // Question.prototype = Object.create(this);
-        // Question.prototype.constructor = Question;
-        // TestController.prototype = Object.create(this);
         return data;
       } else {
       }
@@ -320,28 +371,19 @@ Object.assign(TestService.prototype, {
     }
   },
 });
-//
-//
-//
 
-let prevObj;
-let testController = null;
+let testController = null; //глобально храним не в замыкании функции startTest
 function startTest(user) {
   if (testController)
-    testController.question.deleteTimer(testController.question.timerInterval);
-  testController = new TestController(user);
-  console.log(testController);
+    testController.question.deleteTimer(testController.question.timerInterval); //удаляем таймер т.к он существует в замыкании(если сущесвует)
+  testController = new TestController(user); //создаём новый контроллер при запуске/перезапуске теста
   testController.init();
-  // testController.testInit();
 }
 
 function init() {
-  const user = new User();
-  startTest(user);
-  document.getElementById("restartButton").onclick = () => startTest(user);
-  // el.addEventListener('click', () => {
-  //   console.log("awdawdawd")
-  // })
+  const user = new User(); //useless
+  startTest(user); //инициализация первоначальная
+  document.getElementById("restartButton").onclick = () => startTest(user); //перезапуск теста
 }
 
 init();
